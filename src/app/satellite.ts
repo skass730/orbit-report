ce569fc1b83434b187c0bbc6708eddb26006eb42
skass730 <simonkass730@gmail.com>
2020-09-27 15:41:40 -0500

    Finished part 2

---
+++ src/app/satellite.ts
@@ -0,0 +1,14 @@
+export class Satellite {
+    name: string;
+    type: string;
+    orbitType: string;
+    operational: boolean;
+    launchDate: string;
+    constructor (name: string, type: string, launchDate: string, orbitType: string, operational: boolean){
+        this.name = name;
+        this.type = type;
+        this.launchDate = launchDate;
+        this.orbitType = orbitType;
+        this.operational = operational;
+    }
+}
